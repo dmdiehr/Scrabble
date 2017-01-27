7587648a1a18648f56ae40a5b018073869e6f2ef
David Diehr <dmdiehr@outlook.com>
2017-01-26 19:32:12 -0800

    Adds project files. Adds a Program hello world. Adds basic class files with constructors and preliminary method names.

---
+++ Scrabble/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scrabble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ALL THE WORDS");
+            Game newGame = new Game("dummy");
+            string input = "";
+
+            do
+            {
+                Console.WriteLine("Enter 'words', 'board', or 'exit'");
+                input = Console.ReadLine();
+
+                if (input == "words")
+                {
+                    newGame.DisplayDictionary();
+                }
+
+                if (input == "board")
+                {
+                    newGame.DisplayBoard();
+                }
+            } while (input != "exit");
+
+            Console.WriteLine();
+            Console.WriteLine("*************************THAT'S ALL FOLKS******************************************************");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Scrabble/Tray.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Scrabble
+{
+    public class Tray
+    {
+        //CONSTRUCTOR
+        public Tray(string tray)
+        {
+            if (tray.Length > 7 && tray.Length < 1)
+            {
+                throw new ArgumentException("The Tray must have between 1 and 7 Tiles");
+            }
+
+            List<Tile> newTileList = new List<Tile>();
+
+            foreach (char c in tray)
+            {
+                Tile newTile = new Tile(c);
+                newTileList.Add(newTile);
+            }
+            _tiles = newTileList;
+        }
+
+        //FIELDS
+        private List<Tile> _tiles;
+
+        //METHODS
+    }
+}
