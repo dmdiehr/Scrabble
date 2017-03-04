871b7f3f15aadc556e4f56e60b98e4e982343374
David Diehr <dmdiehr@outlook.com>
2017-03-03 22:28:12 -0800

    Updates a test case.

--- Scrabble.Tests/GameTests.cs
+++ Scrabble.Tests/GameTests.cs
@@ -235,7 +235,7 @@ namespace Scrabble.Tests
 
         [Test]
         [Category("Lots of Results")]
-        [Ignore("Takes forever")]
+        //[Ignore("Takes 15sec for assert 1; 3:16 for assert 4)]
         public void LotsOfPlays()
         {
             //Arrange
@@ -271,16 +271,20 @@ namespace Scrabble.Tests
             //Act
             //List<Play> plays1 = game.FindAllPlays();
 
-            game.SetTray("ABCDEF?");
+            //game.SetTray("ABCDEF?");
             //List<Play> plays2 = game.FindAllPlays();
 
-            game.SetTray("AE??");
-            List<Play> plays3 = game.FindAllPlays();
-            
+            //game.SetTray("AE??");
+            //List<Play> plays3 = game.FindAllPlays();
+
+            game.SetTray("ABCDE??");
+            List<Play> plays4 = game.FindAllPlays();
+
             //Assert
             //Assert.That(plays1.Count, Is.EqualTo(384));
             //Assert.That(plays2.Count, Is.EqualTo(4723));
-            Assert.That(plays3.Count, Is.EqualTo(7562));
+            //Assert.That(plays3.Count, Is.EqualTo(7562));
+            Assert.That(plays4.Count, Is.EqualTo(26734));
         }
 
         [Test]
